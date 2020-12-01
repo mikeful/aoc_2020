68f9be2dc8d6b3e400560dbbb8c3432ea24a84bb
mikeful <mikefulli@gmail.com>
2020-12-01 23:54:25 +0200

    Cleaned up day 1 part 2
    Might be slower 😀

--- 01/part2.ex
+++ 01/part2.ex
@@ -3,13 +3,11 @@ data = File.read!("part1.txt")
 |> Enum.map(&String.trim/1)
 |> Enum.map(&String.to_integer/1)
 
-pairs = for x <- data, y <- data, z <- data, do: [x, y, z]
+pairs = for x <- data, y <- data, z <- data, uniq: true, do: MapSet.new([x, y, z])
 
-correct_pair = Enum.map(pairs, &Enum.sort/1)
-|> Enum.uniq
-|> Enum.filter(fn item -> Enum.sum(item) == 2020 end)
+correct_pair = Enum.filter(pairs, fn item -> Enum.sum(item) == 2020 end)
 |> List.first
 
-result = Enum.reduce(correct_pair, 1, fn item, acc -> acc * item end)
+result = Enum.reduce(MapSet.to_list(correct_pair), 1, fn item, acc -> acc * item end)
 
 IO.inspect(result)
